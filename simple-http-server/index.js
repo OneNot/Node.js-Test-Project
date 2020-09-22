445cb8a1aff92c46898f9f51a494cf2ff173fa47
Toni Hannula <www.1n07@gmail.com>
2020-09-22 15:21:34 +0300

    Express - Static page handling

--- simple-http-server/index.js
+++ simple-http-server/index.js
@@ -1,21 +1,24 @@
-// this is my simple http express-server
-
 const http = require('http');
 const port = 3000;
 const hostname = 'localhost';
 
-
 const express = require('express');
-
 
 // Creates an express app
 const app = express();
 
+// Remember to create public-directory and put eg. index.html & about.html there
+app.use(express.static(__dirname+'/public'));
 
-app.get('/', function(req, res){
-    res.send('Hello World!');
-});
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader( 'Content-type', 'text/html');
+    res.end('<html><head><title>Express Server</title></head><body><p>Error jotain</p></body></html>');
+})
 
+// app.get('/', function(req, res){
+//     res.send('Hello world!');
+// })
 
 const server = http.createServer(app);
-server.listen(port, hostname, () => { console.log(`Server started! Running at http://${hostname}:${port}`)});
+server.listen(port, hostname, () => {console.log(`Server started. Running at http://${hostname}:${port}`)});
