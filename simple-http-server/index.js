ec608ace3a585cee091caa0aaae5614e0bfd970a
Toni Hannula <www.1n07@gmail.com>
2020-09-22 14:10:44 +0300

    Express Hello World

--- simple-http-server/index.js
+++ simple-http-server/index.js
@@ -1,95 +1,21 @@
+// this is my simple http express-server
+
 const http = require('http');
-const fs = require("fs");
-const path = require('path');
-const { runInNewContext } = require('vm');
-
 const port = 3000;
-const hostname = "localhost";
+const hostname = 'localhost';
 
-const server = http.createServer(function(req, res) {
-    console.log(req.headers);
-    console.log("requested: " + req.url);
 
-    //If request method isn't GET...
-    if(req.method != "GET")
-    {
-        SendErrorPage(res, "405");
-    }
-    else
-    {
-        //root url redirect to /index
-        if(req.url == "/")
-            req.url = "/index";
+const express = require('express');
 
-        //build filepath
-        let filePath = "./public" + req.url;
 
-        //get extension
-        let extension = path.extname(filePath);
+// Creates an express app
+const app = express();
 
-        //no extension -> .html
-        if(extension == "")
-        {
-            filePath += ".html";
-            extension = ".html";
-        }
 
-        //if any other extension besides html...
-        if(extension != ".html")
-        {
-            SendErrorPage(res, "404");
-        }
-        else //extension is html
-        {
-            //try to read file given in url
-            fs.readFile(filePath, function(err, data) {
-                if(err) //if reading fails
-                {
-                    SendErrorPage(res, "404");
-                }
-                else //Successfully read the requested page
-                {
-                    //send it
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "text/html");
-                    res.end(data);
-                }
-            });
-        }
-    }
+app.get('/', function(req, res){
+    res.send('Hello World!');
 });
 
-server.listen(port, hostname, () => {console.log("Server started")});
 
-function SendErrorPage(responseObject, pageToShow)
-{
-    if(pageToShow == "404")
-    {
-        responseObject.statusCode = 404;
-        fs.readFile("./public/404.html", function(err, data){
-            //if 404 page not found, send it in plain text
-            if(err)
-            {
-                responseObject.setHeader("Content-Type", "text/plain");
-                responseObject.end("ERROR 404: Page Not Found");
-            }
-            else //send the 404 html page
-            {
-                responseObject.setHeader("Content-Type", "text/html");
-                responseObject.end(data);
-            }
-        });
-    }
-    else if(pageToShow == "405")
-    {
-        responseObject.statusCode = 405;
-        responseObject.setHeader("Content-Type", "text/plain");
-        responseObject.end("ERROR 405: Method Not Allowed");
-    }
-    else
-    {
-        responseObject.statusCode = 418;
-        responseObject.setHeader("Content-Type", "text/plain");
-        responseObject.end("ERROR 418: I'm a Teapot"); //that's a real http response :D
-    }
-}
+const server = http.createServer(app);
+server.listen(port, hostname, () => { console.log(`Server started! Running at http://${hostname}:${port}`)});
